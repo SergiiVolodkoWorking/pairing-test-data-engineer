0715ab2f7e0d02afdb0d4850bea2a22518b40213
svolodko <svolodko@8thlight.com>
2020-01-04 02:13:42 +0000

    Started pipeline implemetation: load_flights and get_covered_days_count steps created

--- pipeline.py
+++ pipeline.py
@@ -1,20 +1,34 @@
 from metaflow import step, FlowSpec
-import os
+import pandas as pd
+
+from transformations import add_date,\
+                            count_column_groups
 
 class AirlinesDataPreparationFlow(FlowSpec):
-    data_floder = os.path.join(os.path.dirname(__file__), 'data')
-    
     @step
     def start(self):
-        print('Flow started');
+        print('Flow started')
+        self.next(self.load_flights)
+
+    @step
+    def load_flights(self):
+        self.flights_df = pd.read_csv('data/flights.csv')
+        self.next(self.get_covered_days_count)
+
+    @step
+    def get_covered_days_count(self):
+        df = add_date(self.flights_df)
+        days_covered = count_column_groups(df, df['date'].dt.date)
+        self.covered_days = days_covered
         self.next(self.end)
 
     @step
     def end(self):
-        print('Flow completed');
+        print('Flow completed')
+        print(self.covered_days)
 
 # Run the pipline by executing:
-# python3 pipeline.py output-dot -Tpng -o graph.png
+# python3 pipeline.py output-dot | dot -Tpng -o graph.png
 # to generate drawing of the pipeline
 if __name__ == '__main__':
     AirlinesDataPreparationFlow()
