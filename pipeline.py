e87a92945b2e05125dcf70b0fedbab5f9539f324
svolodko <svolodko@8thlight.com>
2020-01-07 23:17:23 +0200

    Extended pipeline with get_departure_cities_count step

--- pipeline.py
+++ pipeline.py
@@ -13,19 +13,32 @@ class AirlinesDataPreparationFlow(FlowSpec):
     @step
     def load_flights(self):
         self.flights_df = pd.read_csv('data/flights.csv')
-        self.next(self.get_covered_days_count)
+        self.next(
+            self.get_covered_days_count,
+            self.get_departure_cities_count)
 
     @step
     def get_covered_days_count(self):
         df = add_date(self.flights_df)
         days_covered = count_column_groups(df, df['date'].dt.date)
         self.covered_days = days_covered
+        self.next(self.join)
+
+    @step
+    def get_departure_cities_count(self):
+        flight_origins_count = count_column_groups(self.flights_df, self.flights_df['origin'])
+        self.departure_cities_count = flight_origins_count
+        self.next(self.join)
+
+    @step
+    def join(self, inputs):
         self.next(self.end)
 
     @step
     def end(self):
         print('Flow completed')
-        print(self.covered_days)
+        # print(self.covered_days)
+        # print(self.departure_cities_count)
 
 # Run the pipline by executing:
 # python3 pipeline.py output-dot | dot -Tpng -o graph.png
