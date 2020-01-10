fb5d448c4a9995331f8408363d28ec31fe9a5733
svolodko <svolodko@8thlight.com>
2020-01-10 11:44:43 +0200

    Extending pipeline with the steps for finding biggest delays

--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,8 @@ from metaflow import step, FlowSpec
 import pandas as pd
 
 from transformations import add_date,\
-                            count_column_groups
+                            count_column_groups,\
+                            merge_detasets
 
 class AirlinesDataPreparationFlow(FlowSpec):
     @step
@@ -11,9 +12,15 @@ class AirlinesDataPreparationFlow(FlowSpec):
         self.next(self.load_flights)
 
     @step
+    def load_planes(self):
+        self.planes_df = pd.read_csv('data/planes.csv')
+        self.next(self.merge_flights_and_planes)
+
+    @step
     def load_flights(self):
         self.flights_df = pd.read_csv('data/flights.csv')
         self.next(
+            self.load_planes,
             self.get_covered_days_count,
             self.get_departure_cities_count)
 
@@ -28,6 +35,20 @@ class AirlinesDataPreparationFlow(FlowSpec):
     def get_departure_cities_count(self):
         flight_origins_count = count_column_groups(self.flights_df, self.flights_df['origin'])
         self.departure_cities_count = flight_origins_count
+        self.next(self.join)
+
+    @step
+    def merge_flights_and_planes(self):
+        self.flights_and_planes_df = merge_detasets(self.planes_df, self.flights_df, 'tailnum')
+        self.next(self.find_biggest_delays)
+
+    @step
+    def find_biggest_delays(self):
+        flights_by_planes = self.flights_and_planes_df.groupby(['manufacturer'])
+
+        self.max_departure_delays = flights_by_planes['dep_delay'].max()
+        self.max_arrival_delays = flights_by_planes['arr_delay'].max()
+
         self.next(self.join)
 
     @step
